262ba3c1ade5fc738c5a31e2689a565cebee0d02
abdul1ah33 <study.abdullah2005@gmail.com>
2024-12-31 23:45:12 +0200

    Defines the MealPlan table

---
+++ models/meal_plan.py
@@ -0,0 +1,16 @@
+from . import db
+from datetime import datetime
+
+class MealPlan(db.Model):
+    __tablename__ = 'meal_plans'
+
+    id = db.Column(db.Integer, primary_key=True)
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    meals = db.Column(db.Text, nullable=False)  # JSON string mapping days to recipes
+
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return f"<MealPlan from {self.start_date} to {self.end_date}>"
