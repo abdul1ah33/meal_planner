903b238278a54e470590ef14ee106b68692f0ec1
abdul1ah33 <study.abdullah2005@gmail.com>
2025-01-02 00:11:01 +0200

    d

--- models/recipe.py
+++ models/recipe.py
@@ -1,14 +1,12 @@
-from . import db
+from models import db
 
 class Recipe(db.Model):
     __tablename__ = 'recipes'
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(120), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
     description = db.Column(db.Text, nullable=True)
-    ingredients = db.Column(db.Text, nullable=False)
-    instructions = db.Column(db.Text, nullable=False)
+
+    # Foreign key to users table
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    def __repr__(self):
-        return f"<Recipe {self.title}>"
