85ce28490e2e980e2f690b38c1c3f858cdca802f
abdul1ah33 <study.abdullah2005@gmail.com>
2025-01-01 23:25:23 +0200

    edited again

--- models/__init__.py
+++ models/__init__.py
@@ -9,3 +9,10 @@ def init_db(app):
     db.init_app(app)
     with app.app_context():
         db.create_all()
+
+# Import models here
+from models.user import User  # Import the User model
+
+# This makes `User` accessible when importing `models`
+__all__ = ["db", "init_db", "User"]
+
