5524ca9eac5c76e67f6a64892d3995e2540fc73a
abdul1ah33 <study.abdullah2005@gmail.com>
2025-01-06 21:55:41 +0200

    edited models.meal_planner to add new column for db so that user can choose days of week

--- models/meal_plan.py
+++ models/meal_plan.py
@@ -1,16 +1,23 @@
 from . import db
 from datetime import datetime
+import json
 
 class MealPlan(db.Model):
     __tablename__ = 'meal_plans'
 
     id = db.Column(db.Integer, primary_key=True)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    meals = db.Column(db.Text, nullable=False)
-
+    week_start_date = db.Column(db.Date, nullable=False)
+    meals_data = db.Column(db.Text, nullable=False)  # JSON string storing meal selections
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
 
+    def set_meals(self, meals_dict):
+        """Store meals dictionary as JSON string"""
+        self.meals_data = json.dumps(meals_dict)
+
+    def get_meals(self):
+        """Retrieve meals dictionary from JSON string"""
+        return json.loads(self.meals_data) if self.meals_data else {}
+
     def __repr__(self):
-        return f"<MealPlan from {self.start_date} to {self.end_date}>"
+        return f"<MealPlan for week starting {self.week_start_date}>"
