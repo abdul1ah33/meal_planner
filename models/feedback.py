23d2eb511b5e006ce995f58dad50e44c0df6f9e7
abdul1ah33 <study.abdullah2005@gmail.com>
2025-01-07 16:50:07 +0200

    added feedback table so the user can make feedbacks

---
+++ models/feedback.py
@@ -0,0 +1,15 @@
+from . import db
+from datetime import datetime
+
+class Feedback(db.Model):
+    __tablename__ = 'feedbacks'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)  # 1-5 stars
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    
+    # Relationship with User model
+    user = db.relationship('User', backref='feedbacks')
